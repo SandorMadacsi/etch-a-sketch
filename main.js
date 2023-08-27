0958b1870a8b5a1bc09646ce5a9fc6fd27c1e8f9
Sandor Madacsi <smadacsi@protonmail.com>
2023-08-27 11:52:38 +0100

    Implemented fixed layout

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 
-let units = document.querySelector('div');
-let gridSize = 16;
+let units = document.querySelector('.canvas-container');
+let gridSize = 4;
 for(let i = 0; i < gridSize; i++){
+    let row = document.createElement('div');
+    row.classList.add('unit-row');
     for(let j = 0; j < gridSize; j++){
     
         let unit = document.createElement('div');
         unit.classList.add('unit');
-        unit.setAttribute('style', `width:${window.innerWidth / gridSize}px;`);
-        unit.setAttribute('style', `height:${window.innerHeight / gridSize}px;`);
-        console.log(window.innerHeight / gridSize)
-        units.appendChild(unit);
+        row.appendChild(unit);
     }
+    units.appendChild(row);
 
 
     
