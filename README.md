a308754ab6dcdb55fe809a34bb11ac83051760f9
Sandor Madacsi <smadacsi@protonmail.com>
2023-08-28 22:19:48 +0100

    Fully implement hover effect with resizable canvas

--- README.md
+++ README.md
@@ -5,4 +5,12 @@
 1. Dynamic DOM manipulation with JS callback functions
 1. Appropriate use of flexbox for styling
 
+**Challenges**:
+
+1. Dynamically resizing tiles: Using template literals I was able to set atributes to change width and height based on the parent div's dimentions.
+
+1. In order to achieve the same size for the canvas but changing the tile density I used 'border-box' for the 'box-sizing' attribute which includes the borders in the final width and height.
+
+1. Based on slider input the canvas is redrawn each time a the input changes. I achieved this by clearing the canvas container in the event listener callback function and redraw the canvas with the new input.
+
 
--- main.js
+++ main.js
@@ -3,30 +3,48 @@
 
 let canvas = document.querySelector('.canvas-container');
 let mainContainer = document.querySelector('.container');
-let gridSize = 100;
+
+
+let input=document.querySelector("#input");
+console.log(input.value);
+
+
+
+let gridSize = input.value;
+drawCanvas(gridSize);
+
+
+input.addEventListener("input",function(){
+  
+    gridSize = this.value;
+    sizeText.innerText = `GridSize: ${gridSize}`;
+    canvas.innerHTML="";
+    drawCanvas(gridSize);
+},0);
 
 let sizeText = document.querySelector('h2');
-sizeText.innerText = `GridSize: ${gridSize}`;
 
 
-for(let i = 0; i < gridSize; i++){
-    let row = document.createElement('div');
-    row.classList.add('unit-row');
-
-    for(let j = 0; j < gridSize; j++){
+function drawCanvas(gridSize){
+    for(let i = 0; i < gridSize; i++){
+        let row = document.createElement('div');
+        row.classList.add('unit-row');
     
-        let unit = document.createElement('div');
-        unit.classList.add('unit');
-        unit.setAttribute('style', `width: ${canvas.clientWidth / gridSize}px;
-                                    height: ${canvas.clientHeight / gridSize}px;`);
-        row.appendChild(unit);
+        for(let j = 0; j < gridSize; j++){
+        
+            let unit = document.createElement('div');
+            unit.classList.add('unit');
+            unit.setAttribute('style', `width: ${canvas.clientWidth / gridSize}px;
+                                        height: ${canvas.clientHeight / gridSize}px;`);
+            row.appendChild(unit);
+        }
+        canvas.appendChild(row);
     }
-    canvas.appendChild(row);
+
+    let units = document.querySelectorAll('.unit');
+units.forEach(unit =>  unit.addEventListener('mouseover', fillIn));
 }
 
-
-let units = document.querySelectorAll('.unit');
-units.forEach(unit =>  unit.addEventListener('mouseover', fillIn));
 
 
 //Shading square in
